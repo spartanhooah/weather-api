a04eeda8ae61658515478857535d0800f80c237e
Ben Frey <spartanhooah@gmail.com>
2022-12-20 20:43:37 -0500

    add endpoints for yearly and all data

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def home():
 
 
 @app.route("/api/v1/<station>/<date>")
-def get_data(station, date):
+def station_date(station, date):
     station_id = str(station).zfill(6)
     data = pd.read_csv(f"data_small/TG_STAID{station_id}.txt",
                        skiprows=20,
@@ -24,6 +24,26 @@ def get_data(station, date):
     return {"station": station,
             "date": date,
             "temperature": temperature}
+
+
+@app.route("/api/v1/<station>")
+def station_all(station):
+    station_id = str(station).zfill(6)
+    data = pd.read_csv(f"data_small/TG_STAID{station_id}.txt",
+                       skiprows=20,
+                       parse_dates=["    DATE"])
+
+    return data.to_dict(orient="records")
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def station_year(station, year):
+    station_id = str(station).zfill(6)
+    data = pd.read_csv(f"data_small/TG_STAID{station_id}.txt", skiprows=20)
+    data["    DATE"] = data["    DATE"].astype(str)
+    result = data[data["    DATE"].str.startswith(str(year))]
+
+    return result.to_dict(orient="records")
 
 
 if __name__ == "__main__":
